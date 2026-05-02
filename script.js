ce498c2be78b07948e885aff0dff7777f4380a4f
JajnaseniMishra <jajnasenimishraliza@gmail.com>
2026-05-02 23:27:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function callNow() {
+    alert("Call: +91-XXXXXXXXXX");
+}
+
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({
+        behavior: "smooth"
+    });
+}
